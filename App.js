590d05d912fb450a34a15e4e2467cf8610c9522a
Catalin Miron <mironcatalin@gmail.com>
2020-09-04 16:40:08 +0200

    Fix Android

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 /**
  * Inspiration: https://dribbble.com/shots/8257559-Movie-2-0
- * Twitter: https://twitter.com/mironcatalin
+ *
  */
 import * as React from 'react';
 import {
@@ -9,24 +9,23 @@ import {
   View,
   StyleSheet,
   FlatList,
-  Dimensions,
   Image,
+  Dimensions,
   Animated,
+  TouchableOpacity,
+  Platform,
 } from 'react-native';
 const { width, height } = Dimensions.get('window');
 import { getMovies } from './api';
 import Genres from './Genres';
 import Rating from './Rating';
-import MaskedView from '@react-native-community/masked-view';
-import Svg, { Rect } from 'react-native-svg';
 import { LinearGradient } from 'expo-linear-gradient';
 
-const AnimatedSvg = Animated.createAnimatedComponent(Svg);
-
 const SPACING = 10;
-const ITEM_SIZE = width * 0.72;
-const SPACER_ITEM_SIZE = (width - ITEM_SIZE) / 2;
-const BACKDROP_HEIGHT = height * 0.6;
+const ITEM_SIZE = Platform.OS === 'ios' ? width * 0.72 : width * 0.74;
+const EMPTY_ITEM_SIZE = (width - ITEM_SIZE) / 2;
+const BACKDROP_HEIGHT = height * 0.65;
+
 const Loading = () => (
   <View style={styles.loadingContainer}>
     <Text style={styles.paragraph}>Loading...</Text>
@@ -35,52 +34,48 @@ const Loading = () => (
 
 const Backdrop = ({ movies, scrollX }) => {
   return (
-    <View style={{ position: 'absolute', width, height: BACKDROP_HEIGHT }}>
+    <View style={{ height: BACKDROP_HEIGHT, width, position: 'absolute' }}>
       <FlatList
-        data={movies}
-        keyExtractor={(item) => item.key}
+        data={movies.reverse()}
+        keyExtractor={(item) => item.key + '-backdrop'}
+        removeClippedSubviews={false}
+        contentContainerStyle={{ width, height: BACKDROP_HEIGHT }}
         renderItem={({ item, index }) => {
           if (!item.backdrop) {
             return null;
           }
-
-          const inputRange = [(index - 2) * ITEM_SIZE, (index - 1) * ITEM_SIZE];
-
           const translateX = scrollX.interpolate({
-            inputRange,
-            outputRange: [-width, 0],
+            inputRange: [(index - 2) * ITEM_SIZE, (index - 1) * ITEM_SIZE],
+            outputRange: [0, width],
+            // extrapolate:'clamp'
           });
           return (
-            <MaskedView
-              style={{ position: 'absolute' }}
-              maskElement={
-                <AnimatedSvg
-                  width={width}
-                  height={height}
-                  viewBox={`0 0 ${width} ${height}`}
-                  style={{ transform: [{ translateX }] }}
-                >
-                  <Rect x='0' y='0' width={width} height={height} fill='red' />
-                </AnimatedSvg>
-              }
+            <Animated.View
+              removeClippedSubviews={false}
+              style={{
+                position: 'absolute',
+                width: translateX,
+                height,
+                overflow: 'hidden',
+              }}
             >
               <Image
                 source={{ uri: item.backdrop }}
                 style={{
                   width,
                   height: BACKDROP_HEIGHT,
-                  resizeMode: 'cover',
+                  position: 'absolute',
                 }}
               />
-            </MaskedView>
+            </Animated.View>
           );
         }}
       />
       <LinearGradient
-        colors={['transparent', 'white']}
+        colors={['rgba(0, 0, 0, 0)', 'white']}
         style={{
-          width,
           height: BACKDROP_HEIGHT,
+          width,
           position: 'absolute',
           bottom: 0,
         }}
@@ -95,8 +90,9 @@ export default function App() {
   React.useEffect(() => {
     const fetchData = async () => {
       const movies = await getMovies();
-      // [spacer, ...movies, spacer]
-      setMovies([{ key: 'left-spacer' }, ...movies, { key: 'right-spacer' }]);
+      // Add empty items to create fake space
+      // [empty_item, ...movies, empty_item]
+      setMovies([{ key: 'empty-left' }, ...movies, { key: 'empty-right' }]);
     };
 
     if (movies.length === 0) {
@@ -110,43 +106,41 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <StatusBar hidden />
       <Backdrop movies={movies} scrollX={scrollX} />
+      <StatusBar hidden />
       <Animated.FlatList
         showsHorizontalScrollIndicator={false}
         data={movies}
         keyExtractor={(item) => item.key}
         horizontal
-        contentContainerStyle={{
-          alignItems: 'center',
-        }}
-        snapToInterval={ITEM_SIZE}
-        decelerationRate={0}
         bounces={false}
+        decelerationRate={Platform.OS === 'ios' ? 0 : 0.98}
+        renderToHardwareTextureAndroid
+        contentContainerStyle={{ alignItems: 'center' }}
+        snapToInterval={ITEM_SIZE}
+        snapToAlignment='start'
         onScroll={Animated.event(
           [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-          { useNativeDriver: true }
+          { useNativeDriver: false }
         )}
         scrollEventThrottle={16}
         renderItem={({ item, index }) => {
           if (!item.poster) {
-            return (
-              <View
-                style={{
-                  width: SPACER_ITEM_SIZE,
-                }}
-              />
-            );
+            return <View style={{ width: EMPTY_ITEM_SIZE }} />;
           }
+
           const inputRange = [
             (index - 2) * ITEM_SIZE,
             (index - 1) * ITEM_SIZE,
             index * ITEM_SIZE,
           ];
+
           const translateY = scrollX.interpolate({
             inputRange,
             outputRange: [100, 50, 100],
+            extrapolate: 'clamp',
           });
+
           return (
             <View style={{ width: ITEM_SIZE }}>
               <Animated.View
@@ -154,9 +148,9 @@ export default function App() {
                   marginHorizontal: SPACING,
                   padding: SPACING * 2,
                   alignItems: 'center',
+                  transform: [{ translateY }],
                   backgroundColor: 'white',
                   borderRadius: 34,
-                  transform: [{ translateY }],
                 }}
               >
                 <Image
